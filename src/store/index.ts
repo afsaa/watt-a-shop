b37a6a62204ae8c1643321c95da6527750d63a9e
Andres Saa <andressaa.dev@gmail.com>
2023-07-26 17:12:54 -0500

    Install and setup zustand with devtools and persistance middlewares

---
+++ src/store/index.ts
@@ -0,0 +1,31 @@
+import { create } from 'zustand';
+import { devtools, persist } from 'zustand/middleware';
+import { AppState } from './store.types';
+
+export const useAppStore = create<AppState>()(
+  devtools(
+    persist(
+      (set) => ({
+        shoppingCartCount: 0,
+        increaseShoppingCartCount: (count) => set((state) => ({ shoppingCartCount: state.shoppingCartCount + count })),
+        products: [],
+        setProducts: (products) => set(() => ({ products })),
+        shoppingCartProducts: [],
+        addProductToCart: (newProduct) => set((state) => ({ shoppingCartProducts: { newProduct, ...state.shoppingCartProducts } })),
+        currentProduct: {},
+        setCurrenProduct: (product) => set(() => ({ currentProduct: product })),
+        currentOrder: {},
+        setCurrenOrder: (order) => set(() => ({ currentOrder: order })),
+        orders: [],
+        addOrder: (newOrder) => set((state) => ({ orders: { newOrder, ...state.orders } })),
+        showCheckout: false,
+        setShowCheckout: (show) => set(() => ({ showCheckout: show })),
+        showProductDetail: false,
+        setShowProductDetail: (show) => set(() => ({ showProductDetail: show })),
+      }),
+      {
+        name: 'shop-storage',
+      }
+    )
+  )
+);
---
+++ src/store/store.types.ts
@@ -0,0 +1,37 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export interface Product {
+  id: number;
+  title: string;
+  price: string;
+  category: string;
+  description: string;
+  image: string;
+  savedCO2?: number;
+}
+
+export interface Order {
+  id: number;
+  date: Date | string;
+  products: Product[];
+  totalProducts: number;
+  totalPrice: number;
+}
+
+export interface AppState {
+  shoppingCartCount: number;
+  increaseShoppingCartCount: (count: number) => void;
+  products: Product[];
+  setProducts: (products: Product[]) => void;
+  showProductDetail: boolean;
+  setShowProductDetail: (show: boolean) => void;
+  currentProduct: Product | Record<string, never>;
+  setCurrenProduct: (product: Product) => void;
+  showCheckout: boolean;
+  setShowCheckout: (show: boolean) => void;
+  shoppingCartProducts: Product[];
+  addProductToCart: (product: Product) => void;
+  currentOrder: Order | Record<string, never>;
+  setCurrenOrder: (order: Order) => void;
+  orders: Order[];
+  addOrder: (order: Order) => void;
+}
