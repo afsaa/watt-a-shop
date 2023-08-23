d5363cf8e9fbc914a10fc17f37bc19a3140aa7f8
Andres Saa <andressaa.dev@gmail.com>
2023-08-23 13:26:42 -0500

    Add go back button in current order

--- src/pages/CurrentOrder/index.tsx
+++ src/pages/CurrentOrder/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Layout from '../../components/Layout';
 import { useAppStore } from '../../store';
 
@@ -7,7 +8,14 @@ const CurrentOrder = () => {
   return (
     <Layout>
       <div className="w-96 flex flex-col justify-center items-center dark:text-white">
-        <h1 className="mb-10 text-3xl">Current Order</h1>
+        <div className="w-full mb-10 relative">
+          <Link to="/my-orders" className="flex justify-center items-center absolute left-0 top-2">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+            </svg>
+          </Link>
+          <h1 className="text-3xl ml-32">Current Order</h1>
+        </div>
         <div className="mb-10">
           <div className="flex flex-col justify-between items-center gap-4">
             {currentOrder.products.map((product) => (
