ee0e9b2c32a68d05ba2f2eda68d47758e244f438
Andres Saa <andressaa.dev@gmail.com>
2023-12-22 15:16:16 -0500

    Refactor ProductCard component moving functionality to page level

--- src/components/ProductCard/productCard.tsx
+++ src/components/ProductCard/productCard.tsx
@@ -1,50 +1,10 @@
-import { useAppStore } from '../../store';
-import { Product } from '../../store/store.types';
+import { ProductCardProps } from './productCard.type';
 
-const ProductCard = ({ id, image, title, price, description, category, ...props }: Product): JSX.Element => {
-  const shoppingCartProducts = useAppStore((state) => state.shoppingCartProducts);
-  const addProductToCart = useAppStore((state) => state.addProductToCart);
-  const increaseShoppingCartCount = useAppStore((state) => state.increaseShoppingCartCount);
-  const isProductInCart: boolean = shoppingCartProducts.some((product) => product.id === id);
-  const setShowProductDetail = useAppStore((state) => state.setShowProductDetail);
-  const setShowCart = useAppStore((state) => state.setShowCart);
-  const setCurrenProduct = useAppStore((state) => state.setCurrenProduct);
-
-  const handleShowProductDetail: () => void = () => {
-    setCurrenProduct({
-      id,
-      title,
-      price,
-      description,
-      image,
-      category,
-      ...props,
-    });
-    setShowCart(false);
-    setShowProductDetail(true);
-  };
-
-  const handleAddProductToCart: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void = (event) => {
-    event.stopPropagation();
-    if (isProductInCart) {
-      return;
-    }
-    addProductToCart({
-      id,
-      title,
-      price,
-      description,
-      image,
-      category,
-      ...props,
-    });
-    increaseShoppingCartCount(1);
-    setShowProductDetail(false);
-    setShowCart(true);
-  };
+const ProductCard = ({ handleShowProductDetail, handleAddProductToCart, ...props }: ProductCardProps): JSX.Element => {
+  const { category, description, image, price, title, isProductInCart } = props;
 
   return (
-    <div className="w-60 h-auto rounded-lg text-black dark:text-white cursor-pointer" onClick={() => handleShowProductDetail()}>
+    <div className="w-60 h-auto rounded-lg text-black dark:text-white cursor-pointer" onClick={() => handleShowProductDetail(props)}>
       <figure className="relative w-full h-4/5 rounded-lg">
         <span className="absolute bottom-0 left-0 m-2 p-1 rounded-lg bg-slate-300 dark:bg-slate-600 dark:text-white opacity-80 capitalize">{category}</span>
         <img className="w-full h-full object-cover rounded-lg" src={image} alt={description} />
@@ -52,7 +12,7 @@ const ProductCard = ({ id, image, title, price, description, category, ...props 
           className={`absolute top-0 right-0 w-7 h-7 m-1 p-1 flex justify-center items-center rounded-full bg-slate-400 dark:bg-slate-800 dark:text-white text-black ${
             isProductInCart ? 'opacity-50' : ''
           }`}
-          onClick={(e) => handleAddProductToCart(e)}
+          onClick={(e) => handleAddProductToCart(e, props)}
         >
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,18 @@
 /* eslint-disable react-hooks/exhaustive-deps */
+import { ProductCard } from '@/components';
 import * as React from 'react';
 import { useEffect } from 'react';
-import { ProductCard } from '@/components';
 import { useAppStore } from '../../store';
 import { Product } from '../../store/store.types';
 import { filterBy } from '../../utils';
 
 const Home = (): JSX.Element => {
+  const cart = useAppStore((state) => state.shoppingCartProducts);
+  const addProductToCart = useAppStore((state) => state.addProductToCart);
+  const increaseCartCount = useAppStore((state) => state.increaseShoppingCartCount);
+  const setShowProductDetail = useAppStore((state) => state.setShowProductDetail);
+  const setShowCart = useAppStore((state) => state.setShowCart);
+  const setCurrentProduct = useAppStore((state) => state.setCurrenProduct);
   const products: Product[] | undefined = useAppStore((state) => state.products);
   const setProducts = useAppStore((state) => state.setProducts);
   const setFilteredProducts = useAppStore((state) => state.setFilteredProducts);
@@ -35,14 +41,49 @@ const Home = (): JSX.Element => {
     setTitleQuery(event.target.value);
   };
 
+  const handleShowProductDetail: (product: Product) => void = (product) => {
+    setCurrentProduct(product);
+    setShowCart(false);
+    setShowProductDetail(true);
+  };
+
+  const handleAddProductToCart: (event: React.MouseEvent<HTMLDivElement, MouseEvent>, product: Product) => void = (event, product) => {
+    event.stopPropagation();
+    const isProductInCart: boolean = cart.some((foundProduct) => foundProduct.id === product.id);
+
+    if (isProductInCart) {
+      return;
+    }
+    addProductToCart(product);
+    increaseCartCount(1);
+    setShowProductDetail(false);
+    setShowCart(true);
+  };
+
   const renderProducts = () => {
     if (filteredProducts.length > 0) {
-      return filteredProducts.map((product) => <ProductCard key={product.id} {...product} />);
+      return filteredProducts.map((filteredProduct) => {
+        const isProductInCart: boolean = cart.some((foundProduct) => foundProduct.id === filteredProduct.id);
+
+        return (
+          <ProductCard
+            key={filteredProduct.id}
+            {...filteredProduct}
+            isProductInCart={isProductInCart}
+            handleShowProductDetail={handleShowProductDetail}
+            handleAddProductToCart={handleAddProductToCart}
+          />
+        );
+      });
     }
     if (titleQuery !== '' && filteredProducts.length === 0) {
       return <div className="col-span-full text-black dark:text-white">We don't have results for this search</div>;
     }
-    return products.map((product) => <ProductCard key={product.id} {...product} />);
+    return products.map((product) => {
+      const isProductInCart: boolean = cart.some((foundProduct) => foundProduct.id === product.id);
+
+      return <ProductCard key={product.id} {...product} isProductInCart={isProductInCart} handleShowProductDetail={handleShowProductDetail} handleAddProductToCart={handleAddProductToCart} />;
+    });
   };
 
   return (
