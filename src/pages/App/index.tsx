57d7315575e44621b99ed4fcf08d246781683502
Andres Saa <afsaa@utp.edu.co>
2024-08-27 10:54:12 -0500

    Separate responsibilities between App and Product detail

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -1,16 +1,10 @@
 import { Cart, Layout, Navbar, ProductDetail } from '@/components';
 import { AppRoutes } from '@/routes';
-import { useAppStore } from '@/store';
-import { Product } from '@/store/store.types';
 import { checkMode } from '@/utils';
 import { useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 function App(): JSX.Element {
-  const showProductDetail: boolean = useAppStore((state) => state.showProductDetail);
-  const setShowProductDetail: (show: boolean) => void = useAppStore((state) => state.setShowProductDetail);
-  const currentProduct = useAppStore((state) => state.currentProduct);
-
   useEffect(() => {
     checkMode();
     return () => {};
@@ -22,7 +16,7 @@ function App(): JSX.Element {
       <Layout>
         <AppRoutes />
       </Layout>
-      <ProductDetail showProductDetail={showProductDetail} handleShowProductDetail={setShowProductDetail} {...(currentProduct as Product)} />
+      <ProductDetail />
       <Cart />
     </BrowserRouter>
   );
